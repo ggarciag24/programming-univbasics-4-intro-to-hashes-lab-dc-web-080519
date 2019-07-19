8b91daf54b6294afb3c13fa22b60b130d8b84eae
German Garcia <ggarciag.business@outlook.com>
2019-07-19 13:45:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,9 +35,9 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash.include?(key)
      hash[key] += 1
-     return hash[key]
+     puts hash[key]
   else
     hash[key] = 1
-    return hash[key]
+    puts hash[key]
   end  
 end
